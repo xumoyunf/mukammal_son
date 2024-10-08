1249a137edf38728ad755b1aed49e8f1618983c0
xumoyunf <xakimjonovxumoyun7@gmail.com>
2024-10-08 21:53:44 +0500

    The variable num is changed to number

--- bin/mukammal.dart
+++ bin/mukammal.dart
@@ -9,38 +9,38 @@ void main(List<String> arguments) {
 
 bool checkNumber() {
   stdout.write('\nSon kiriting: ');
-  var num = stdin.readLineSync().toString();
-  if (int.tryParse(num).runtimeType == int) {
-    if (isEven(int.parse(num))) {
+  var number = stdin.readLineSync().toString();
+  if (int.tryParse(number).runtimeType == int) {
+    if (isEven(int.parse(number))) {
       print('Juft son');
     } else {
       print('Toq son');
     }
-    checkPrime(int.parse(num));
-    checkPerfectNumber(int.parse(num));
+    checkPrime(int.parse(number));
+    checkPerfectNumber(int.parse(number));
     stdout.write("*******************************************************");
     return true;
   } else {
-    num != 'q' ? stdout.write("Xatolik: Butun son kiriting") : null;
+    number != 'q' ? stdout.write("Xatolik: Butun son kiriting\n") : null;
     stdout.write("*******************************************************");
-    return num == 'q' ? false : true;
+    return number == 'q' ? false : true;
   }
 }
 
-isEven(int num) {
-  if (num % 2 == 0) {
+isEven(int number) {
+  if (number % 2 == 0) {
     return true;
   }
   return false;
 }
 
-checkPrime(int num) {
-  if (num <= 1) {
+checkPrime(int number) {
+  if (number <= 1) {
     print('1, 0 va manfiy sonlar tub va murakkab sonlar qatoriga kirmaydi!!!');
   } else {
     List<int> multipliers = [];
-    for (int i = 2; i < num; i++) {
-      if (num % i == 0) {
+    for (int i = 2; i < number; i++) {
+      if (number % i == 0) {
         multipliers.add(i);
       } else {
         continue;
@@ -50,17 +50,17 @@ checkPrime(int num) {
   }
 }
 
-checkPerfectNumber(int num) {
-  if (num <= 0) {
+checkPerfectNumber(int number) {
+  if (number <= 0) {
     print('0 va manfiy sonlar mukammal son bo\'la olmaydi!!!');
   } else {
     int sum = 0;
-    for (int i = 1; i < num; i++) {
-      if (num % i == 0) {
+    for (int i = 1; i < number; i++) {
+      if (number % i == 0) {
         sum += i;
       }
     }
-    if (sum == num) {
+    if (sum == number) {
       print('Mukammal son');
     } else {
       print('Mukammal bo\'lmagan son');
